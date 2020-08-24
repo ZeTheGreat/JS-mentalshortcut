0df5a59faac4f1a8135ec91f56e835ddb50ac8ac
José Roberto Alves Junior <josealves@tecsus.com.br>
2020-08-23 22:06:41 -0300

    :)

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 document.addEventListener("click", (ev) => {
   const target = ev.target;
-  if (target.tagName.toUpperCase() === "BUTTON") {
-    if (target.id === "olaButton") {
-      const ancor = document.getElementById("olaAncor");
-      ancor.innerHTML = "Ola MUNDOOO";
+  if (target.tagName.toUpperCase() === "LI") {
+    if (target.innerText.toUpperCase() === "HOME") {
+      makeRequest("/html/home/home.html").then((response) =>
+        mapingRoot(response)
+      );
+    } else if (target.innerText.toUpperCase() === "CONTATO") {
+      makeRequest("/html/contato/contato.html").then((response) =>
+        mapingRoot(response)
+      );
     }
-  } else if (target.tagName.toUpperCase() === "A") {
-    console.log("CLICO NA ANCOR");
   }
 });
 
-function TableBlock() {}
+async function makeRequest(link, config = {}) {
+  try {
+    const response = await fetch(link, config);
+    if (response.status < 200 && response.status >= 300)
+      throw new Error("Error request");
+    const text = await response.text();
+    return text;
+  } catch (er) {
+    return er;
+  }
+}
+
+function mapingRoot(html) {
+  document.getElementById("root").innerHTML = html;
+}
